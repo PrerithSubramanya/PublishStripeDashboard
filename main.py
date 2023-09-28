edc9ca028792cf7e196eb15758276f4a2f0ed8c9
Prerith Subramanya <prerithsubramanya@gmail.com>
2023-09-28 17:08:40 +1000

    implemented a simple dashboard

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+
+import json
+
+st.title("Stripe API Terminal Locations")
+
+# Using the secrets in the app
+api_key = st.secrets["api_key"]
+device_group_key = st.secrets["device_group_key"]
+device_group_value = st.secrets["device_group_value"]
+
+# User input for the location ID
+location_id = st.text_input("Enter the terminal location ID:")
+
+# Button to trigger the API call
+if st.button("Fetch Location Details"):
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Stripe-Version": "2020-08-27; terminal_deploy_api_beta=v1"
+    }
+    data = {
+        f"device_deploy_groups[{device_group_key}]": device_group_value
+    }
+    response = requests.post(
+        f"https://api.stripe.com/v1/terminal/locations/{location_id}",
+        headers=headers,
+        data=json.dumps(data)
+    )
+
+    if response.status_code == 200:
+        st.write(response.json())
+    else:
+        st.write(f"Error: {response.status_code}")
+        st.write(response.text)
